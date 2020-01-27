56f833bf85b047f8bd70e8867ff28c4af8b4a5ae
Mark Sayers <marksayers46@icloud.com>
2020-01-26 19:18:39 -0600

    Finished up stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -46,10 +46,101 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+
+
+function cardMaker(items) {
+  let card = document.createElement('div');
+  let img = document.createElement('img');
+  let cardInfo = document.createElement('div');
+  let header = document.createElement('h3');
+  let userName = document.createElement('p');
+  let location = document.createElement('p');
+  let profileDiv = document.createElement('div');
+  let profile = document.createElement('p');
+  let anchor = document.createElement('a');
+  let followers = document.createElement('p');
+  let following = document.createElement('p');
+  let bio = document.createElement('p');
+  let button = document.createElement('button');
+  let gitDiv = document.createElement('div')
+  let id = document.createElement('p');
+  let node = document.createElement('p');
+  let repos = document.createElement('p');
+  let created = document.createElement('p');
+  let updated = document.createElement('p');
+
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(header);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profileDiv);
+  profileDiv.appendChild(profile);
+  profileDiv.appendChild(anchor);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  cardInfo.appendChild(button);
+  cardInfo.appendChild(gitDiv);
+  gitDiv.appendChild(id);
+  gitDiv.appendChild(node);
+  gitDiv.appendChild(repos);
+  gitDiv.appendChild(created);
+  gitDiv.appendChild(updated);
+
+  profileDiv.style.display = 'flex';
+  anchor.style.fontSize = 12;
+  anchor.style.paddingLeft = 5;
+
+  card.classList.add('card');
+  cardInfo.classList.add('cardInfo');
+  header.classList.add('name');
+  userName.classList.add('username');
+  gitDiv.classList.add('gitDiv');
+
+  img.src = items.avatar_url;
+  header.textContent = items.name;
+  userName.textContent = items.login;
+  location.textContent = items.location;
+  profile.textContent = `Profile:`;
+  anchor.textContent = items.html_url;
+  anchor.href = items.html_url;
+  followers.textContent = `Followers: ${items.followers}`;
+  following.textContent = `Following: ${items.following}`;
+  bio.textContent = items.bio;
+  button.textContent = `GitHub Details`;
+  id.textContent = items.id;
+  node.textContent = `Node ID: ${items.node_id}`;
+  repos.textContent = `Public Repos: ${items.public_repos}`;
+  created.textContent = `Created on: ${items.created_at}`;
+  updated.textContent = `Last Updated: ${items.updated_at}`;
+
+  button.addEventListener('click', (e) => {
+    gitDiv.classList.toggle('open')
+  })
+
+  return card;
+}
+
+
+let array = ['https://api.github.com/users/marksayers46', 
+'https://api.github.com/users/troopaloop8', 
+'https://api.github.com/users/pjose92',
+'https://api.github.com/users/eaczechova',
+'https://api.github.com/users/jailang',
+'https://api.github.com/users/aaronmatson1',
+'https://api.github.com/users/DonnaBallew']
+
+let body = document.querySelector('.cards');
+
+array.forEach(element => {
+axios.get(element)
+  .then(result => {
+    console.log(result);
+    body.appendChild(cardMaker(result.data));
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+})
+
